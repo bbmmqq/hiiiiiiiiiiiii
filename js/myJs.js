7d9d260b55868af6b79481e71f3cdc50e970f3b1
bbmmqq <buiquan0706@gmail.com>
2026-04-10 17:07:36 +0700

    hehe

--- js/myJs.js
+++ js/myJs.js
@@ -141,7 +141,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/messages/e2ee/t/6920742444651849";
+            window.location = "https://www.messenger.com/e2ee/t/6920742444651849";
           },
         });
       }

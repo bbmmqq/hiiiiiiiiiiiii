19eee06279368c7abc49d09af71be77c4d9f3b9a
bbmmqq <buiquan0706@gmail.com>
2026-04-10 17:00:24 +0700

    hehe

--- js/myJs.js
+++ js/myJs.js
@@ -29,10 +29,10 @@ $(document).ready(function () {
   $("#text4").html(textConfig.text4);
   $("#no").html(textConfig.text5);
   $("#yes").html(textConfig.text6);
-    var audio = new Audio("sound/bg-music.mp3");
-    audio.play();
   function firstQuestion() {
     $(".content").hide();
+    var audio = new Audio("sound/bg-music.mp3");
+    audio.play();
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,

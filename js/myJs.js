c456fd4ae644d286f2b502daa613548414a2d35c
bbmmqq <buiquan0706@gmail.com>
2026-04-10 17:05:31 +0700

    hehe

--- js/myJs.js
+++ js/myJs.js
@@ -141,7 +141,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/messages/e2ee/t/6920742444651849";
           },
         });
       }
